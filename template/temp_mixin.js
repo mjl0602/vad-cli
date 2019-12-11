7e6f608aa67c4066d7a3b1ea7ae97cab35d3d231
MJL <675531696@qq.com>
2019-12-11 20:54:25 +0800

    添加了对数组形式的支持

--- template/temp_mixin.js
+++ template/temp_mixin.js
@@ -62,7 +62,7 @@ export default {
       this.listLoading = true;
       try {
         let res = await this.source.all(this.query);
-        if (res.total) {
+        if (res.hasProperty("total")) {
           this.list = res.data;
           this.query.total = res.total;
         } else {
