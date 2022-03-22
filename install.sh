41ee28c0e456f66b2fbc099913441a44922676ca
马嘉伦 <majialun@majialundeMacBook-Air.local>
2022-03-22 12:52:58 +0800

    [0.0.1+1]😘Feat: 修正了安装脚本

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 pub global activate --source path ./
-rm -rf ./.dart_tool/pub/bin/template
-cp -r ./template ./.dart_tool/pub/bin/template
+rm -rf ./.dart_tool/pub/bin/template/
+mkdir -p ./.dart_tool/pub/bin/template/
+cp -r ./template/ ./.dart_tool/pub/bin/template/
