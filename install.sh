57ca9d7e0bfc60ece4348762a8cb81194a4bff3f
马嘉伦 <majialun@majialundeMac-Studio.local>
2022-05-11 10:40:39 +0800

    [0.0.1+1]😘Feat: 更新了打包脚本

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pub global activate --source path ./
+dart pub global activate --source path ./
 rm -rf ./.dart_tool/pub/bin/template/
 mkdir -p ./.dart_tool/pub/bin/template/
 cp -r ./template/ ./.dart_tool/pub/bin/template/
